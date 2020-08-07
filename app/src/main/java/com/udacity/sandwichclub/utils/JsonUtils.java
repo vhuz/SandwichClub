84b6e62eb44196da1c6d1d052328d7131c0a2f8c
Abacus7 <vladhuz@yahoo.com>
2020-08-06 23:09:05 -0400

    feat: implemented parseSandwichJson
    implemented parseSandwichJson in module JsonUtils.java
    The data from input json string is getting parsed and added to Sandwich object (based on model in module Sandwich.java)
    Still need to work with string lists to parse them correctly.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,91 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.udacity.sandwichclub.DetailActivity;
+import com.udacity.sandwichclub.MainActivity;
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    // parsing data from string-array sandwich_details
+    // (located in strings.xml)
+
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+
+        Sandwich parsedData = new Sandwich();
+
+        JSONObject rootJSON_OBJECT = new JSONObject(json);
+        JSONObject rawJSON_OBJECT = rootJSON_OBJECT.getJSONObject("name");
+
+        // setting Main Name
+        String mainName = rawJSON_OBJECT.getString("mainName");
+        parsedData.setMainName(mainName);
+
+        // setting 'alsoKnownAs' name
+//        List<String> list = null;
+//        JSONArray secondName = rawJSON_OBJECT.getJSONArray("alsoKnownAs");
+//
+//        if (secondName.length() != 0 & secondName != null) {
+//            for (int i = 0; i < secondName.length(); i++) {
+//                list.add(secondName.getString(i));
+//            }
+//        }
+
+//        if (list != null) {
+//            parsedData.setAlsoKnownAs(list);
+//        } else {
+//            parsedData.setAlsoKnownAs(Arrays.asList(""));
+//        }
+
+        parsedData.setAlsoKnownAs(Arrays.asList("sup1", "sup2", "sup3"));
+
+
+        // setting 'description'
+        parsedData.setDescription(rootJSON_OBJECT.getString("description"));
+
+        // setting 'placeOfOrigin'
+        parsedData.setPlaceOfOrigin(rootJSON_OBJECT.getString("placeOfOrigin"));
+
+        // setting 'image'
+        parsedData.setImage(rootJSON_OBJECT.getString("image"));
+
+        // setting 'ingredients'
+
+//        List<String> list2 = null;
+//        JSONArray ingredientsArray = rootJSON_OBJECT.getJSONArray("ingredients");
+//
+//        if (ingredientsArray.length() != 0 && ingredientsArray != null) {
+//            for (int i = 0; i < ingredientsArray.length(); i++) {
+//                String currentName = ingredientsArray.getString(i);
+//                list2.add(currentName);
+//            }
+//        }
+//
+//        if (list2 != null) {
+//            parsedData.setIngredients(list2);
+//        } else {
+//            parsedData.setIngredients(Arrays.asList(""));
+//        }
+
+        parsedData.setIngredients(Arrays.asList("sup1", "sup2", "sup3"));
+
+        return parsedData;
     }
 }
